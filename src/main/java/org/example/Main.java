d19c3f298634c88b25fd6c1ac32db350d8b1cddd
Guterresthomas <guterresmikael@gmail.com>
2023-10-10 15:42:58 -0300

    java string length counter

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main (String[] args) {
+        String string1 = "kaosasjdsajkd";
+        String string2 = "xyz";
+
+        String result = longestWithAnAnnouncement(string1, string2, "Today is someone's birthday!");
+        System.out.println("The longest string is " + result);
+    }
+
+    public static String longestWithAnAnnouncement (String x, String y, String ann) {
+        System.out.println("Announcement" + ann);
+        if (x.length() > y.length()) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+}
